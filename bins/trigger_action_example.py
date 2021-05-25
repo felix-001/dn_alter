e1bba34e9a8d65f0193a43dd4eba4e35a0bd9f58
liyq <liyuanquan@qiniu.com>
2021-05-25 10:57:07 +0800

    update

--- bins/trigger_action_example.py
+++ bins/trigger_action_example.py
@@ -14,40 +14,32 @@ from urllib import parse
 import time
 
 
-wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=92f4334a-906a-4394-b975-d9bba071f19d"    # 测试机器人1号
+wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=92f4334a-906a-4394-b975-d9bba071f19d"
 
 class TriggerActionExample(OnDemandTriggerAction):
 
     def do_handle_init(self, packet):
-        """
-        :param packet:
-        :return:
-        """
         if packet.body().contains_metadata():
             metadata = packet.body().metadata()
             logging.info("metadata = %s", metadata)
+
     def send_msg(self, content):
-        """并发送指定信息"""
+        """发送指定信息到微信机器人"""
         data = json.dumps({"msgtype": "text", "text": {"content": content, "mentioned_list":["@all"]}})
         r = requests.post(wx_url, data, auth=('Content-Type', 'application/json'))
 
     def do_handle_events(self, events):
         length = len(events)
         event_name = ""
-        event_display_id = ""
         evt_id = ""
-        params = {}
         evt_time = ""
-        eventConditionRecord = ""
         alertDescription = ""
         logging.info("event length = {0}".format(length))
+
         if length > 0:
             event_name = events[0].eventName
-            event_display_id = events[0].eventDisplayId
-            params = events[0].params
             evt_id = events[0].eventId
             evt_time = events[0].eventTime
-            eventConditionRecord = events[0].eventConditionRecord
             alertDescription = events[0].alertDescription
 
         content = "告警: " + event_name + "\n"
@@ -57,7 +49,7 @@ class TriggerActionExample(OnDemandTriggerAction):
         if len(events[0].additionContents) > 0:
             additions = events[0].additionContents
             query = parse.urlencode({'queryString' : additions[0].spl})
-            url = "http://qvs-pdr.qnlinking.com/logdb/search/log?dataLimit=-1&mode=smart&preset=m5&preview=true"
+            url = "http://qvs-pdr.qnlinking.com/logdb/search/log?dataLimit=-1&mode=smart&preview=true"
             url += "&" + query
             timeArray = time.strptime(evt_time, "%Y-%m-%d %H:%M:%S")
             start = str(int(time.mktime(timeArray))*1000)
@@ -65,7 +57,6 @@ class TriggerActionExample(OnDemandTriggerAction):
             end_ms = int(round(time.time() * 1000))
             url += "&end=" + str(end_ms)
             content += "事件日志链接: " + url + "\n"
-            content += "原始日志: " + json.dumps(additions[0].contents[0])
         # do send events to other platform
         self.send_msg(content)
         return content
